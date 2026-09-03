d9e3a58f4bbc4a6fc34804f59a79729c2eecc56d
Shuvo Biswas <shuvobiswas800@gmail.com>
2026-09-04 02:17:53 +0600

    Update server.js

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: "https://studynook-client-uiuh.vercel.app",
     credentials: true,
   })
 );
